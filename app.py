eb9def637586ccfbfd632e338ee35b0c96e22fce
otf <otf@me.com>
2022-08-21 06:47:22 +0900

    Add scraping for ping-pong.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, Response
 import os
-import time
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
@@ -13,9 +15,19 @@ def text_stream():
     def generate():
         for n in range(61):
             yield f'{str(n)} sec<br>'
-            time.sleep(1)
+            sleep(1)
 
     return Response(generate())
+
+@app.route("/")
+def index():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    with webdriver.Chrome(options = options) as driver:
+        driver.get('https://example-heroku-flask-selenium.herokuapp.com/ping')
+        return driver.page_source
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = os.environ.get('PORT'))
