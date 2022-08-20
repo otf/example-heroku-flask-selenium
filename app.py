0e0f6a1886d00d10c11461551a1797421c9b9fc8
otf <otf@me.com>
2022-08-20 20:34:54 +0900

    Add a poll route that returns stream response.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, Response
 import os
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/poll")
+def poll():
+    def generate():
+        yield '<ul>'
+        i = 0
+        while True:
+            i += 1
+            yield f'<li>{str(i)}</li>'
+            time.sleep(1)
+
+    return Response(generate())
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = os.environ.get('PORT'))
