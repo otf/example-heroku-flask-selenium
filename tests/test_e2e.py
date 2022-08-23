01a8eda6bd170b2ec40579ac929f85df94493c1b
otf <otf@me.com>
2022-08-23 10:48:08 +0900

    Make it possible to set base url for e2e test.

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -4,10 +4,11 @@ from playwright.sync_api import Playwright, APIRequestContext
 
 @pytest.fixture(scope='session')
 def api_request_context(
-        playwright: Playwright
+        playwright: Playwright,
+        base_url: str
         ) -> Generator[APIRequestContext, None, None]:
     request_context = playwright.request.new_context(
-            base_url = 'https://example-heroku-flask-selenium.herokuapp.com/')
+            base_url = base_url)
     yield request_context
     request_context.dispose()
 
