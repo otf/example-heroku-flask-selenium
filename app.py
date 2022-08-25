e871fe668be624bc0943634f411fb0012ac4ce06
otf <otf@me.com>
2022-08-25 13:09:39 +0900

    Revert "Use threading instead of generator for preventing heroku timeout."
    This reverts commit abade68a09e6e0e39fa43b5af1e3dcd86b102bdc.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, Response, request
 import os
 from time import sleep
 from datetime import datetime
-from threading import Thread
-from queue import Queue
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,18 +21,7 @@ def page(page_number):
 
 @app.route("/")
 def index():
-    chunk_queue = Queue()
-    generate_lines_thread = None
-
-    def generate_chunks():
-        while not chunk_queue.empty() or generate_lines_thread.is_alive():
-            if chunk_queue.empty():
-                yield ''
-                sleep(10)
-            else:
-                yield chunk_queue.get_nowait()
-        
-    def generate_lines(url_root):
+    def generate(url_root):
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
@@ -47,14 +34,11 @@ def index():
                     ))
 
             for n in range(60 * 3):
-                chunk_queue.put(f'{elm(By.ID, "txt").text},{datetime.now().isoformat()}\n')
+                yield f'{elm(By.ID, "txt").text},{datetime.now().isoformat()}\n'
                 elm(By.ID, 'btn-next').click()
 
-    generate_lines_thread = Thread(target = generate_lines, args = (request.url_root,))
-    generate_lines_thread.start()
-
     return Response(
-            response = generate_chunks(),
+            response = generate(request.url_root),
             mimetype = 'text/csv',
             headers = [('Content-Disposition', 'attachment; filename=pages.csv')])
 
