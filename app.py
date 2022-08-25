5e776c8184b4b93ac5de39c982bbeb88dd121e84
otf <otf@me.com>
2022-08-25 12:55:23 +0900

    Use threading instead of generator for preventing heroku timeout.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, Response, request
 import os
 from time import sleep
 from datetime import datetime
+from threading import Thread
+from queue import Queue
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,7 +23,18 @@ def page(page_number):
 
 @app.route("/")
 def index():
-    def generate(url_root):
+    chunk_queue = Queue()
+    generate_lines_thread = None
+
+    def generate_chunks():
+        while not chunk_queue.empty() or generate_lines_thread.is_alive():
+            if chunk_queue.empty():
+                yield ''
+                sleep(10)
+            else:
+                yield chunk_queue.get_nowait()
+        
+    def generate_lines(url_root):
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
@@ -34,11 +47,14 @@ def index():
                     ))
 
             for n in range(60 * 3):
-                yield f'{elm(By.ID, "txt").text},{datetime.now().isoformat()}\n'
+                chunk_queue.put(f'{elm(By.ID, "txt").text},{datetime.now().isoformat()}\n')
                 elm(By.ID, 'btn-next').click()
 
+    generate_lines_thread = Thread(target = generate_lines, args = (request.url_root,))
+    generate_lines_thread.start()
+
     return Response(
-            response = generate(request.url_root),
+            response = generate_chunks(),
             mimetype = 'text/csv',
             headers = [('Content-Disposition', 'attachment; filename=pages.csv')])
 
